c0113a3fdeadc5e9484238a9f4f36f667931c46f
David Silva <david@davidafsilva.pt>
2022-03-13 19:19:09 +0000

    adds support for artifacts versioning and publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,33 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import pl.allegro.tech.build.axion.release.domain.ChecksConfig
+import pl.allegro.tech.build.axion.release.domain.RepositoryConfig
+import pl.allegro.tech.build.axion.release.domain.TagNameSerializationConfig
+
+// register repositories for both buildscript and application
+buildscript.repositories.registerRepositories()
+repositories.registerRepositories()
 
 plugins {
-    kotlin("jvm") version "1.6.10"
+    kotlin("jvm")
+    id("pl.allegro.tech.build.axion-release")
+    `maven-publish`
+    signing
 }
 
 group = "pt.davidafsilva.jvm.prettystring"
-version = "0.0.1-SNAPSHOT"
-
-repositories {
-    mavenCentral()
+scmVersion {
+    tag(closureOf<TagNameSerializationConfig> {
+        prefix = "v"
+        versionSeparator = ""
+    })
+    checks(closureOf<ChecksConfig> {
+        isUncommittedChanges = false
+    })
+    repository(closureOf<RepositoryConfig> {
+        pushTagsOnly = true
+    })
 }
+version = scmVersion.version
 
 dependencies {
     implementation(kotlin("stdlib"))
@@ -21,17 +39,97 @@ dependencies {
 }
 
 val javaVersion = JavaVersion.VERSION_1_8
-java {
+configure<JavaPluginExtension> {
     sourceCompatibility = javaVersion
     targetCompatibility = javaVersion
+    withSourcesJar()
+    withJavadocJar()
 }
 
-tasks.withType<KotlinCompile>().configureEach {
-    kotlinOptions {
-        jvmTarget = javaVersion.toString()
+configure<PublishingExtension> {
+    repositories {
+        maven {
+            name = "Sonatype"
+            val repository = when {
+                version.toString().endsWith("-SNAPSHOT") -> "/content/repositories/snapshots/"
+                else -> "/service/local/staging/deploy/maven2/"
+            }
+            setUrl("https://s01.oss.sonatype.org/$repository")
+            credentials {
+                username = System.getenv("OSSRH_USER")
+                password = System.getenv("OSSRH_TOKEN")
+            }
+        }
+    }
+
+    publications {
+        create<MavenPublication>("artifacts") {
+            from(components["java"])
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+            pom {
+                val githubRepoUrl = "https://github.com/davidafsilva/pretty-string"
+
+                name.set(project.name)
+                description.set("https://github.com/davidafsilva/pretty-string")
+                url.set("https://github.com/davidafsilva/pretty-string")
+                inceptionYear.set("2022")
+                licenses {
+                    license {
+                        name.set("MIT")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("davidafsilva")
+                        name.set("David Silva")
+                        url.set("https://github.com/davidafsilva")
+                    }
+                }
+                scm {
+                    val githubRepoCheckoutUrl = "$githubRepoUrl.git"
+
+                    connection.set(githubRepoCheckoutUrl)
+                    developerConnection.set(githubRepoCheckoutUrl)
+                    url.set(githubRepoUrl)
+                }
+            }
+        }
     }
 }
 
-tasks.withType<Test>().configureEach {
-    useJUnitPlatform()
+configure<SigningExtension> {
+    val signingGpgKey: String? by project
+    val signingGpgKeyId: String? by project
+    val signingGpgKeyPassword: String? by project
+    if (signingGpgKey != null && signingGpgKeyId != null && signingGpgKeyPassword != null) {
+        useInMemoryPgpKeys(signingGpgKeyId, signingGpgKey, signingGpgKeyPassword)
+    }
+
+    sign(publishing.publications.getByName("artifacts"))
+}
+
+tasks {
+    withType<KotlinCompile>().configureEach {
+        kotlinOptions {
+            jvmTarget = javaVersion.toString()
+        }
+    }
+
+    withType<Test> {
+        useJUnitPlatform()
+    }
+
+    withType<Jar> {
+        from("${projectDir}/LICENSE") {
+            rename("LICENSE", "META-INF/LICENSE.txt")
+        }
+    }
+}
+
+fun RepositoryHandler.registerRepositories() {
+    mavenLocal()
+    mavenCentral()
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,10 @@
+pluginManagement {
+    val kotlinVersion: String by settings
+    val axionVersion: String by settings
+    plugins {
+        kotlin("jvm") version kotlinVersion
+        id("pl.allegro.tech.build.axion-release") version axionVersion
+    }
+}
+
 rootProject.name = "pretty-string"
